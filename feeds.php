2476f21134e01e2ba93f4c1f2385bf3b045632c3
sandhiya22 <sandhiya22.websitica@gmail.com>
2016-11-21 15:01:23 +0530

    Create feeds.php

---
+++ feeds.php
@@ -0,0 +1,35 @@
+<?php
+header("Content-type: text/xml");
+
+echo '<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+<channel>';
+
+ 
+ 
+$json = $_GET["json"];
+$google = file_get_contents($json);
+ $date = date('Y-m-d',strtotime("-1 days"));
+$final = json_decode($google,true);
+shuffle($final);
+$i = 1;
+foreach($final as $lol)
+{
+
+$lol = str_replace("&","&amp;",$lol);
+$lol = "https://".$_SERVER['HTTP_HOST']."/red.php?url=$lol";
+
+echo '
+  <item>
+    <title>'.$_SERVER['HTTP_HOST'].' Link '.$i.'</title>
+    <link>'.$lol.'</link>
+    <description>'.$_SERVER['HTTP_HOST'].' description '.$i.'</description>
+  </item>
+';
+ $i++;
+}
+?>
+ 
+</channel>
+
+</rss>
