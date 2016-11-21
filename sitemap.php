f84940ed4643555352ee51a2af91a03a904904c5
sandhiya22 <sandhiya22.websitica@gmail.com>
2016-11-21 14:29:33 +0530

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -1,14 +1,19 @@
-<?php
+<?xml version="1.0" encoding="UTF-8"?>
+
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+ 
+ <?php
 $json = $_GET["json"];
 $google = file_get_contents($json);
-
+ $date = date('Y-m-d',strtotime("-1 days"));
 $final = json_decode($google,true);
-
+shuffle($final);
 foreach($final as $lol)
 {
-echo $lol."<br>";
-
-
+echo "<url> <loc>$lol</loc> <lastmod>2005-01-01</lastmod> <changefreq>daily</changefreq> <priority>1.0</priority> </url>";
 }
 
 ?>
+ 
+</urlset> 
