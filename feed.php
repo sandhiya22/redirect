7bead593e27946588b95674f52d68f70d1944c87
sandhiya22 <sandhiya22.websitica@gmail.com>
2016-11-21 15:54:56 +0530

    Update feed.php

--- feed.php
+++ feed.php
@@ -1,10 +1,9 @@
 <?php
+//header('Content-Type: application/xml');
 header('Content-Type: application/rss+xml; charset=utf-8');
-?>
-&#60;?xml version='1.0' encoding='ISO-8859-1'?&#62;
+echo '<?xml version="1.0" encoding="utf-8"?>
 <rss version='2.0'>
 <channel>';
-
  
  
 $json = $_GET["json"];
