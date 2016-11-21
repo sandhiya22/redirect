3d0c19fe2ca45df34b8316b9e5c277d2cb8caa6f
sandhiya22 <sandhiya22.websitica@gmail.com>
2016-11-21 16:04:15 +0530

    Update feed.php

--- feed.php
+++ feed.php
@@ -1,29 +1,37 @@
 <?php
-//header('Content-Type: application/xml');
-header('Content-Type: application/rss+xml; charset=utf-8');
-echo '<?xml version="1.0" encoding="utf-8"?>';
-echo "<rss version='2.0'>";
-echo '<channel>';
+    header("Content-Type: application/rss+xml; charset=ISO-8859-1");
+ 
+ 
+    $rssfeed = '<?xml version="1.0" encoding="ISO-8859-1"?>';
+    $rssfeed .= '<rss version="2.0">';
+    $rssfeed .= '<channel>';
+    $rssfeed .= '<title>My RSS feed</title>';
+    $rssfeed .= '<link>http://'.$_SERVER['HTTP_HOST'].'</link>';
+    $rssfeed .= '<description>  RSS feed</description>';
+    $rssfeed .= '<language>en-us</language>';
+  
  
  
 $json = $_GET["json"];
 $google = file_get_contents($json);
- $date = date('Y-m-d',strtotime("-1 days"));
+$date = date('Y-m-d',strtotime("-1 days"));
 $final = json_decode($google,true);
 shuffle($final);
 $i = 1;
 foreach($final as $lol)
 {
 
-$lol = str_replace("&","&amp;",$lol);
-echo '<item>
-    <title>'.$_SERVER['HTTP_HOST'].' Link '.$i.'</title>
-    <link>'.$lol.'</link>
-    <description>'.$_SERVER['HTTP_HOST'].' description '.$i.'</description>
-  </item>
-  ';
- $i++;
-}
+ 
+        $rssfeed .= '<item>';
+        $rssfeed .= '<title>'.$_SERVER['HTTP_HOST'].' Link '.$i.'</title>';
+        $rssfeed .= '<description>'.$_SERVER['HTTP_HOST'].' description '.$i.'</description>';
+        $rssfeed .= '<link>' . $lol . '</link>';
+        $rssfeed .= '<pubDate>' . date("D, d M Y H:i:s O", strtotime($date)) . '</pubDate>';
+        $rssfeed .= '</item>';
+    }
+ 
+    $rssfeed .= '</channel>';
+    $rssfeed .= '</rss>';
+ 
+    echo $rssfeed;
 ?>
-</channel>
-</rss>
