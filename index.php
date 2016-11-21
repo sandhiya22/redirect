d299a4b32226d3930a1711b88ab57b3c4df5e13a
sandhiya22 <sandhiya22.websitica@gmail.com>
2016-11-21 15:38:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,20 +5,5 @@ echo $_GET["wassup"];
 ?>
 
 <head>
-  <meta name="google-site-verification" content="083mEuhoqevLtcghDrKu3fLrq6txt2cJWkThygUG-pM" />
-  <meta name="google-site-verification" content="083mEuhoqevLtcghDrKu3fLrq6txt2cJWkThygUG-pM" />
-  <meta name="google-site-verification" content="083mEuhoqevLtcghDrKu3fLrq6txt2cJWkThygUG-pM" />
-  <meta name="google-site-verification" content="083mEuhoqevLtcghDrKu3fLrq6txt2cJWkThygUG-pM" />
-  <meta name="google-site-verification" content="083mEuhoqevLtcghDrKu3fLrq6txt2cJWkThygUG-pM" />
-  <meta name="google-site-verification" content="ORSzDZtiyrTkLHV7VHnNNPEFwrxsTzmg6KUGpxJVRyY" />
-  <meta name="google-site-verification" content="3h1TKmiWK2jwCydZ9tJECJX3uuBjidJs6pnMkOxoxBs" />
-  <meta name="google-site-verification" content="mGGd7b3omXG9hgDUWJ1Bm9x4W4A_Fx7M5Qey280vuD0" />
-  <meta name="google-site-verification" content="9dr1Ot6Ir5YEnWE7trrZs8Sly3yTRhFXJFD_Mp5Z-3A" />
-  <meta name="google-site-verification" content="9dr1Ot6Ir5YEnWE7trrZs8Sly3yTRhFXJFD_Mp5Z-3A" />
-  <meta name="google-site-verification" content="ORSzDZtiyrTkLHV7VHnNNPEFwrxsTzmg6KUGpxJVRyY" />
-  <meta name="google-site-verification" content="xUe4dmV0vEmde3DpFjI6kRrLI33HN88EtdNlnLfdxHw" />
-  <meta name="google-site-verification" content="CHOkkOdZHqSYP18DzCewBS9jddf4wwIBGcYLuNgZEqQ" />
-  <meta name="google-site-verification" content="BMo_A2a0Pw3d5xV__U36Lhn6T_-unWip4s6feFz7iP8" />
-  <meta name="google-site-verification" content="CHOkkOdZHqSYP18DzCewBS9jddf4wwIBGcYLuNgZEqQ" />
-  <meta name="google-site-verification" content="K1t7Qtz1Q4TdCNcbwgfvKMUUDlcx-hNaf00aORV2V-s" />
+ <meta name="google-site-verification" content="NbYSz5Q7bT12y6FT7lS8EBnhxSCsAfVWwON5_TvECyA" />
 </head>
