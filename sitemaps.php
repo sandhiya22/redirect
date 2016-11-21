c1766fdb54e15f04d371010f07af5c660f98f005
sandhiya22 <sandhiya22.websitica@gmail.com>
2016-11-21 14:49:57 +0530

    Create sitemaps.php

---
+++ sitemaps.php
@@ -0,0 +1,29 @@
+<?php
+header("Content-type: text/xml");
+
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+ 
+ 
+$json = $_GET["json"];
+$google = file_get_contents($json);
+ $date = date('Y-m-d',strtotime("-1 days"));
+$final = json_decode($google,true);
+shuffle($final);
+foreach($final as $lol)
+{
+
+$lol = str_replace("&","&amp;",$lol);
+$lol = "https://".$_SERVER['HTTP_HOST']."?url=$lol";
+echo '
+<url> 
+<loc>'.$lol.'</loc> 
+<lastmod>'.$date.'</lastmod> 
+<changefreq>daily</changefreq> 
+<priority>1.0</priority>
+ </url>';
+}
+?>
+ 
+</urlset> 
