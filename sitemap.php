2950f2df7fc23854476d172432e6b1b1372e33b8
sandhiya22 <sandhiya22.websitica@gmail.com>
2016-11-21 14:38:39 +0530

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -1,9 +1,11 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?php
+header("Content-type: text/xml");
 
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
  
- <?php
+ 
 $json = $_GET["json"];
 $google = file_get_contents($json);
  $date = date('Y-m-d',strtotime("-1 days"));
@@ -11,9 +13,16 @@ $final = json_decode($google,true);
 shuffle($final);
 foreach($final as $lol)
 {
-echo "<url> <loc>$lol</loc> <lastmod>2005-01-01</lastmod> <changefreq>daily</changefreq> <priority>1.0</priority> </url>";
-}
 
+$lol = str_replace("&","&amp;",$lol);
+echo '
+<url> 
+<loc>'.$lol.'</loc> 
+<lastmod>'.$date.'</lastmod> 
+<changefreq>daily</changefreq> 
+<priority>1.0</priority>
+ </url>';
+}
 ?>
  
 </urlset> 
